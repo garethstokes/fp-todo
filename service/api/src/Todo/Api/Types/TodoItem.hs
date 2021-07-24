416eb468cb2ca65e963274c6169159dab7c0f41b
gareth <gareth.stokes@paidright.io>
2021-07-24 13:17:28 +1000

    dont use Text for Api properties, use the Core types instead

--- service/api/src/Todo/Api/Types/TodoItem.hs
+++ service/api/src/Todo/Api/Types/TodoItem.hs
@@ -1,32 +1,35 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Todo.Api.Types.TodoItem
-  ( TodoItemIndex
-  , mkTodoItemIndex
-  )
-where
+module Todo.Api.Types.TodoItem (
+  TodoItemIndex,
+  mkTodoItemIndex,
+) where
 
+import Control.Lens (view)
 import Data.Aeson (ToJSON)
 import Data.Aeson.TH
 import Data.Text (Text)
-import Todo.Core.TodoItem.Types (TodoItem, renderId, renderName)
+import Todo.Core.TodoItem.Types (TodoItem, tiId, tiName)
+import qualified Todo.Core.TodoItem.Types as TodoItem
 
 data TodoItemIndex = TodoItemIndex
-  { _tliId    :: Text
-  , _tliName  :: Text
+  { _tliId :: TodoItem.Id
+  , _tliName :: TodoItem.Name
   }
   deriving (Eq, Show)
 
-$(deriveJSON 
-    defaultOptions 
-    { fieldLabelModifier = drop 4, omitNothingFields = True } 
+$( deriveJSON
+    defaultOptions
+      { fieldLabelModifier = drop 4
+      , omitNothingFields = True
+      }
     ''TodoItemIndex
  )
 
 mkTodoItemIndex :: [TodoItem] -> [TodoItemIndex]
 mkTodoItemIndex = map toIndex
-  where toIndex ti = 
-          TodoItemIndex
-            (renderId ti)
-            (renderName ti)
-    
+ where
+  toIndex ti =
+    TodoItemIndex
+      (view tiId ti)
+      (view tiName ti)
--- service/core/src/Todo/Core/TodoItem/Types.hs
+++ service/core/src/Todo/Core/TodoItem/Types.hs
@@ -1,19 +1,21 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Todo.Core.TodoItem.Types where
 
 import Control.Lens.TH (makeLenses)
+import Data.Aeson (FromJSON, ToJSON)
 import Data.Text (Text)
 import GHC.Generics (Generic)
 
-newtype Id = 
-  Id Text 
-  deriving (Eq, Ord, Show, Generic)
+newtype Id
+  = Id Text
+  deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
 
-newtype Name = 
-  Name Text 
-  deriving (Eq, Ord, Show, Generic)
+newtype Name
+  = Name Text
+  deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
 
 data Status
   = TodoPending
@@ -21,15 +23,14 @@ data Status
   | TodoUnknown
   deriving (Eq, Ord, Show, Generic)
 
-data TodoItem = 
-  TodoItem 
-    { _tiId :: Id
-    , _tiName :: Name
-    , _tiStatus :: Status
-    }
-    deriving (Eq, Ord, Show, Generic)
+data TodoItem = TodoItem
+  { _tiId :: Id
+  , _tiName :: Name
+  , _tiStatus :: Status
+  }
+  deriving (Eq, Ord, Show, Generic)
 
 makeLenses ''TodoItem
 
 renderId (TodoItem (Id todoId) _ _) = todoId
-renderName (TodoItem _ (Name todoName) _ ) = todoName
+renderName (TodoItem _ (Name todoName) _) = todoName
